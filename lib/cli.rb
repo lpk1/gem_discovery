2db666eb8ea732dd803df54a308e07d4f7c07d56
lenny knoop <lennartpknoop@gmail.com>
2018-12-13 16:43:33 +0100

    some rearranging of files and advances in the creation of the pdf

--- lib/cli.rb
+++ lib/cli.rb
@@ -3,21 +3,13 @@ require 'prawn'
 
 class CLI < Thor
   desc "notification", "prints a notification on mac os"
-  def notification(
-      title = "Testing notifications! And their lengths!",
-      message = "This is an amazing test notification! Guess i have a lot more space to write text End!"
-    )
-    TerminalNotifier.notify(
-      message,
-      title: title,
-      open: 'https://news.ycombinator.com',
-      contentImage: 'https://img.icons8.com/color/1600/hacker-news.png'
-    )
+  def notification(title = "Testing notifications! And their lengths!", message = "This is an amazing test notification! Guess i have a lot more space to write text End!")
+    TerminalNotifier.notify(message, :title => title, :open => 'https://news.ycombinator.com', :contentImage => 'https://img.icons8.com/color/1600/hacker-news.png')
   end
 
   desc "generate_pdf", "generates a simple hello.pdf in the local directory"
   def generate_pdf
-    Prawn::Document.generate("function_results/hello.pdf") do
+    Prawn::Document.generate("hello.pdf") do
       stroke_axis
 
       text "the cursor is here: #{cursor}"
@@ -31,8 +23,6 @@ class CLI < Thor
 
       move_cursor_to 50
       text "on the last move the cursor went directly to: #{cursor}"
-
-      start_new_page
     end
   end
 end
